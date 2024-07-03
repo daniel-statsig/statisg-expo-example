e1d35e93d1922bc0172d9993322b3621e719a481
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2024-07-02 17:49:30 -0700

    first

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StatsigProviderExpo,
+  useFeatureGate,
+  warmCachingFromAsyncStorage,
+} from "@statsig/expo-bindings";
+import { StatsigClient } from "@statsig/js-client";
+import { useMemo } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function Content() {
+  const gate = useFeatureGate("a_gate");
+  return (
+    <Text>
+      a_gate: {String(gate.value)} {gate.details.reason}
+    </Text>
+  );
+}
+
+function useMyStatsigClient() {
+  return useMemo(() => {
+    const client = new StatsigClient(
+      "client-rfLvYGag3eyU0jYW5zcIJTQip7GXxSrhOFN69IGMjvq",
+      { userID: "a-user" }
+    );
+
+    const warming = warmCachingFromAsyncStorage(client);
+
+    return { client, warming };
+  });
+}
 
 export default function App() {
+  const { warming, client } = useMyStatsigClient();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatsigProviderExpo client={client} cacheWarming={warming}>
+        <Content />
+      </StatsigProviderExpo>
     </View>
   );
 }
@@ -13,8 +43,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
